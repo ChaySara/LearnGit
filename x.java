f78f5018d6104d08502dcb82dd295764956f50e7
SariYona <sari100375@gmail.com>
2025-06-17 13:27:32 +0300

    code in java

---
+++ x.java
@@ -0,0 +1,9 @@
+public class x {
+	public static void main(String[] args) {
+		int x = 3;
+        int y = 4;
+        int z = 5;
+        int result = x + y + z;
+        System.out.println("The sum of x, y, and z is: " + result);
+    }
+}
